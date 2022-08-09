269b04a532821f22d129c44a1be7362db0f74fe6
webdev0516 <reactdev0516@gmail.com>
2022-08-09 16:14:44 -0400

    fix

--- index.ts
+++ index.ts
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5016;
 // });
 app.use(express.static('client/build'));
  app.get('*', (req: Request, res: Response) => {
- res.sendFile(path.join(__dirname + '/client/build/index.html'));
+ res.sendFile(path.join('/client/build/index.html'));
 });
 
 // routing
--- routes/calculator.route.ts
+++ routes/calculator.route.ts
@@ -6,8 +6,8 @@ const router = Router();
 const calcMain = (req: Request, res: Response) => {
     const { total, next, operation } = req.query as any;
     
-    let totalNum: number | null = isNaN(total) ? null : Number(total);
-    let nextNum: number | null = isNaN(next) ? null : Number(next);
+    const totalNum: number | null = isNaN(total) ? null : Number(total);
+    const nextNum: number | null = isNaN(next) ? null : Number(next);
     
     if (nextNum == null) {
         res.send(totalNum ? totalNum.toString() : "0");
@@ -16,14 +16,19 @@ const calcMain = (req: Request, res: Response) => {
     switch (operation) {
         case "plus":
             res.send((totalNum as number + nextNum).toString());
+            break;
         case "minus":
             res.send((totalNum as number - nextNum).toString());
+            break;
         case "times":
             res.send((totalNum as number * nextNum).toString());
+            break;
         case "divide":
             res.send((totalNum as number / nextNum).toString());
+            break;
         default: {
             res.send(nextNum.toString());
+            break;
         }
     }
 };
